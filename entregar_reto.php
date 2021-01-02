555cbf4b5d1e36748a36bb48f2ad34feff4c8a0e
poliestireno@gmail.com <poliestireno@gmail.com>
2021-01-02 10:28:34 +0100

    Texto del tamaño máximo de subida de fichero a 20MB

--- entregar_reto.php
+++ entregar_reto.php
@@ -9,6 +9,10 @@ if((!isset($_SESSION['alogin']))||(strlen($_SESSION['alogin'])==0))
 } 
 ?>
 
+
+
+
+
 <!doctype html>
 <html lang="en" class="no-js">
 
@@ -274,8 +278,7 @@ echo "Swal.fire({
 
 
 						<h2 class="text-center text-bold mt-2x">Entregar Reto <?php echo $nombreReto?></h2>
-                        <h4 class="text-center text-bold mt-2x"><?php echo " [fecha límite de entrega ".$filaTarea['FECHA_LIMITE']."]"?></h4>
-                        <h4 class="text-center text-bold mt-2x">(Vas a hacer la entrega número <?php echo $numeroEntregas.(($entregaFueraTiempo)?"<b style='color:red'> [entrega fuera de tiempo por ".$sDiferenciaFueraTiempo."] </b>":"")?>)</h4>
+                        <h4 class="text-center text-bold mt-2x"><?php echo " [fecha límite de entrega: ".$filaTarea['FECHA_LIMITE']."]"?><br/>[Tamaño límite de la entrega: 20MB]<br/>(Vas a hacer la entrega número <?php echo $numeroEntregas.(($entregaFueraTiempo)?"<b style='color:red'> [entrega fuera de tiempo por ".$sDiferenciaFueraTiempo."] </b>":"")?>)</h4>
                         <div class="hr-dashed"></div>
 						<div class="well row pt-2x pb-3x bk-light text-center">
                          <form method="post" class="form-horizontal" enctype="multipart/form-data" name="regform" id="regform">
