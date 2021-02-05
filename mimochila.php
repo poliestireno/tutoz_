7d2b44ab4b6a49109cc51ed3104c7a8a96740350
poliestireno@gmail.com <poliestireno@gmail.com>
2021-02-05 02:24:32 +0100

    dado en la mochila

--- mimochila.php
+++ mimochila.php
@@ -103,7 +103,197 @@ shuffle($aRespuestas2);
 	<!-- Admin Stye -->
 	<link rel="stylesheet" href="css/style.css">
 
-	<style>
+
+<style type="text/css">
+
+
+.dice {
+  align-items: center;
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(8rem, 1fr));
+  grid-template-rows: auto;
+  justify-items: center;
+  padding: 2rem;
+  perspective: 600px;
+}
+.die-list {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  height: 6rem;
+  list-style-type: none;
+  transform-style: preserve-3d;
+  width: 6rem;
+}
+.even-roll {
+  transition: transform 1.5s ease-out;
+}
+.odd-roll {
+  transition: transform 1.25s ease-out;
+}
+.die-item {
+  background-color: #fefefe;
+  box-shadow: inset -0.35rem 0.35rem 0.75rem rgba(0, 0, 0, 0.3),
+    inset 0.5rem -0.25rem 0.5rem rgba(0, 0, 0, 0.15);
+  display: grid;
+  grid-column: 1;
+  grid-row: 1;
+  grid-template-areas:
+    "one two three"
+    "four five six"
+    "seven eight nine";
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  height: 100%;
+  padding: 2rem;
+  width: 100%;
+}
+.dot {
+  align-self: center;
+  background-color: #676767;
+  border-radius: 50%;
+  box-shadow: inset -0.15rem 0.15rem 0.25rem rgba(0, 0, 0, 0.5);
+  display: block;
+  height: 1.25rem;
+  justify-self: center;
+  width: 1.25rem;
+}
+.even-roll[data-roll="1"] {
+  transform: rotateX(360deg) rotateY(720deg) rotateZ(360deg);
+}
+.even-roll[data-roll="2"] {
+  transform: rotateX(450deg) rotateY(720deg) rotateZ(360deg);
+}
+.even-roll[data-roll="3"] {
+  transform: rotateX(360deg) rotateY(630deg) rotateZ(360deg);
+}
+.even-roll[data-roll="4"] {
+  transform: rotateX(360deg) rotateY(810deg) rotateZ(360deg);
+}
+.even-roll[data-roll="5"] {
+  transform: rotateX(270deg) rotateY(720deg) rotateZ(360deg);
+}
+.even-roll[data-roll="6"] {
+  transform: rotateX(360deg) rotateY(900deg) rotateZ(360deg);
+}
+.odd-roll[data-roll="1"] {
+  transform: rotateX(-360deg) rotateY(-720deg) rotateZ(-360deg);
+}
+.odd-roll[data-roll="2"] {
+  transform: rotateX(-270deg) rotateY(-720deg) rotateZ(-360deg);
+}
+.odd-roll[data-roll="3"] {
+  transform: rotateX(-360deg) rotateY(-810deg) rotateZ(-360deg);
+}
+.odd-roll[data-roll="4"] {
+  transform: rotateX(-360deg) rotateY(-630deg) rotateZ(-360deg);
+}
+.odd-roll[data-roll="5"] {
+  transform: rotateX(-450deg) rotateY(-720deg) rotateZ(-360deg);
+}
+.odd-roll[data-roll="6"] {
+  transform: rotateX(-360deg) rotateY(-900deg) rotateZ(-360deg);
+}
+[data-side="1"] {
+  transform: rotate3d(0, 0, 0, 90deg) translateZ(4rem);
+}
+[data-side="2"] {
+  transform: rotate3d(-1, 0, 0, 90deg) translateZ(4rem);
+}
+[data-side="3"] {
+  transform: rotate3d(0, 1, 0, 90deg) translateZ(4rem);
+}
+[data-side="4"] {
+  transform: rotate3d(0, -1, 0, 90deg) translateZ(4rem);
+}
+[data-side="5"] {
+  transform: rotate3d(1, 0, 0, 90deg) translateZ(4rem);
+}
+[data-side="6"] {
+  transform: rotate3d(1, 0, 0, 180deg) translateZ(4rem);
+}
+[data-side="1"] .dot:nth-of-type(1) {
+  grid-area: five;
+}
+[data-side="2"] .dot:nth-of-type(1) {
+  grid-area: one;
+}
+[data-side="2"] .dot:nth-of-type(2) {
+  grid-area: nine;
+}
+[data-side="3"] .dot:nth-of-type(1) {
+  grid-area: one;
+}
+[data-side="3"] .dot:nth-of-type(2) {
+  grid-area: five;
+}
+[data-side="3"] .dot:nth-of-type(3) {
+  grid-area: nine;
+}
+[data-side="4"] .dot:nth-of-type(1) {
+  grid-area: one;
+}
+[data-side="4"] .dot:nth-of-type(2) {
+  grid-area: three;
+}
+[data-side="4"] .dot:nth-of-type(3) {
+  grid-area: seven;
+}
+[data-side="4"] .dot:nth-of-type(4) {
+  grid-area: nine;
+}
+[data-side="5"] .dot:nth-of-type(1) {
+  grid-area: one;
+}
+[data-side="5"] .dot:nth-of-type(2) {
+  grid-area: three;
+}
+[data-side="5"] .dot:nth-of-type(3) {
+  grid-area: five;
+}
+[data-side="5"] .dot:nth-of-type(4) {
+  grid-area: seven;
+}
+[data-side="5"] .dot:nth-of-type(5) {
+  grid-area: nine;
+}
+[data-side="6"] .dot:nth-of-type(1) {
+  grid-area: one;
+}
+[data-side="6"] .dot:nth-of-type(2) {
+  grid-area: three;
+}
+[data-side="6"] .dot:nth-of-type(3) {
+  grid-area: four;
+}
+[data-side="6"] .dot:nth-of-type(4) {
+  grid-area: six;
+}
+[data-side="6"] .dot:nth-of-type(5) {
+  grid-area: seven;
+}
+[data-side="6"] .dot:nth-of-type(6) {
+  grid-area: nine;
+}
+
+
+@media (min-width: 900px) {
+  .dice {
+    perspective: 1300px;
+  }
+}
+.botonAnchoTotal {
+  display: block;
+  width: 100%;
+  border: none;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+}
+
+
 	.errorWrap {
     padding: 10px;
     margin: 0 0 20px 0;
@@ -121,6 +311,7 @@ shuffle($aRespuestas2);
     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
 }
 		</style>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script type="text/javascript">
 	function testRapido()
 	{       	
@@ -135,6 +326,47 @@ shuffle($aRespuestas2);
 function init()
 	{ 
 	}
+
+function rollDice() {
+  const dice = [...document.querySelectorAll(".die-list")];
+  dice.forEach(die => {
+    toggleClasses(die);
+    die.dataset.roll = getRandomNumber(1, 6);
+  });
+}
+
+function toggleClasses(die) {
+  die.classList.toggle("odd-roll");
+  die.classList.toggle("even-roll");
+}
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+		function mostratDado() 
+		{
+			Swal.fire({
+	  html:
+	    '<div class="dice"> <ol class="die-list even-roll" data-roll="1" id="die-1"> <li class="die-item" data-side="1"> <span class="dot"></span> </li> <li class="die-item" data-side="2"> <span class="dot"></span> <span class="dot"></span> </li> <li class="die-item" data-side="3"> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> </li> <li class="die-item" data-side="4"> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> </li> <li class="die-item" data-side="5"> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> </li> <li class="die-item" data-side="6"> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> <span class="dot"></span> </li> </ol> </div> <br/><br/><br/><button type="button" id="roll-button" class="botonAnchoTotal" onclick="rollDice()" >Lanzalooo</button>'
+	    	,
+				  showConfirmButton: false,
+				  showCloseButton: false,
+				  showCancelButton: false,
+				  focusConfirm: false,
+				  
+				})
+
+							
+
+
+		}
+
+
+
 </script>
 
 </head>
@@ -165,50 +397,70 @@ function init()
 <input type="hidden" name="idPreguntaNext" id="idPreguntaNext" value=""/>
 <input type="hidden" name="ids_respuestas" id="ids_respuestas" value="<?php echo implode(",", $aRespuestas2); ?>"/>
 
-								<div class="panel panel-default col-sm-2">
-									<div class="panel-heading">Test Rápido</div>
-									<div class="panel-body">
+		<div class="panel panel-default col-sm-2">
+		<div class="panel-heading">Test Rápido</div>
+		<div class="panel-body">
 
-<div class="form-group">
-	<div class="col-sm-10">
+		<div class="form-group">
+		<div class="col-sm-10">
 		<label class="control-label">Test Rápido</label>
-	</div>
-</div>
-<div class="form-group">
+		</div>
+		</div>
+		<div class="form-group">
 
-<div class="col-md-4">
-  <a onclick="testRapido();" class="btn btn-info btn-outline btn-wrap-text">Hacer test</a>
-</div> 
+		<div class="col-md-4">
+		<a onclick="testRapido();" class="btn btn-info btn-outline btn-wrap-text">Hacer test</a>
+		</div> 
 
-	</div>
+		</div>
 
-									</div>
+		</div>
 
 
-								</div>
+		</div>
 
-								<div class="panel panel-default col-sm-2 col-sm-offset-1">
-									<div class="panel-heading">Test Lento</div>
-									<div class="panel-body">
+		<div class="panel panel-default col-sm-2 col-sm-offset-1">
+		<div class="panel-heading">Test Lento</div>
+		<div class="panel-body">
 
-<div class="form-group">
-	<div class="col-sm-10">
+		<div class="form-group">
+		<div class="col-sm-10">
 		<label class="control-label">Test Lento</label>
-	</div>
-</div>
-<div class="form-group">
+		</div>
+		</div>
+		<div class="form-group">
 
-<div class="col-md-4">
-  <a onclick="testLento();" class="btn btn-info btn-outline btn-wrap-text">Hacer test</a>
-</div> 
+		<div class="col-md-4">
+		<a onclick="testLento();" class="btn btn-info btn-outline btn-wrap-text">Hacer test</a>
+		</div> 
 
-	</div>
+		</div>
 
-									</div>
+		</div>
 
 
-								</div>
+		</div>
+		<div class="panel panel-default col-sm-2 col-sm-offset-1">
+		<div class="panel-heading">Lanzar Dado</div>
+		<div class="panel-body">
 
+		<div class="form-group">
+		<div class="col-sm-10">
+		<label class="control-label">Lanzar Dado</label>
+		</div>
+		</div>
+		<div class="form-group">
+
+		<div class="col-md-4">
+		<a onclick="mostratDado();" class="btn btn-info btn-outline btn-wrap-text">Suerte!!!</a>
+		</div> 
+
+		</div>
+
+		</div>
+
+
+		</div>
 
 </form>
 									</div>
